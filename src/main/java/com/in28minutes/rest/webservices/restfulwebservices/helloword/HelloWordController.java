75c033c209d26336c34c215537a109929f5e758a
ita <itamustamin@gmail.com>
2020-01-16 06:43:41 +0700

    Internationalization for RESTful Services

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWordController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWordController.java
@@ -1,14 +1,23 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloword;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//Controller
+@Configuration
 @RestController
 public class HelloWordController {
+	
+	@Autowired
+	private MessageSource pesanSource; //messageSource: a programmatically registered singleton, so nama method di service adlh "messageSource"
 	
 	//method: "Hello Word"
 	//GET
@@ -28,6 +37,11 @@ public class HelloWordController {
 	@GetMapping(path = "/helloWord-path/{name}")
 	public HelloWordBean helloWordPath(@PathVariable String name) {
 		return new HelloWordBean(String.format("Hello Word, %s", name));
+	}
+	
+	@GetMapping(path = "/helloWord-internalizationalized")
+	public String helloWordInternalizationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return pesanSource.getMessage("good.morning.message",null, locale);
 	}
 
 }
